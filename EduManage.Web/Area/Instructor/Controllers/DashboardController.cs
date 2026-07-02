540b8d66c179d82c7f9d2f446a1458fb1c69c938
essamprog <essampro2002@gmail.com>
2026-07-02 07:43:16 +0300

    Save my current work admin

---
+++ EduManage.Web/Area/Instructor/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduManage.Web.Area.Instructor.Controllers
+{
+    [Area("Instructor")]
+    [Authorize(Roles = "Instructor")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
